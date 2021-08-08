ba43dc97f22080375e56d89ac768330d898093b4
amdouni-chouaib <chouaibamdouni@gmail.com>
2021-08-08 17:11:21 +0100

    learn component

---
+++ learn-components/src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Fullname from '../../style/fullname.css'
+const FullName = () => {
+    return (
+        <div>
+            <h1>Amdouni Chouaib</h1>
+        </div>
+    )
+}
+
+export default FullName
